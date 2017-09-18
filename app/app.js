bd2776e1e7463cef087bcbbae807cbbafd626f97
enochdone <enochdone@gmail.com>
2017-09-18 21:43:37 +0000

    changes

---
+++ app/app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var dataFile =require('./data/data.json');
+var dFile=require('./data/Profile.json');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+
+	  var info='';
+	  dFile.speakers.forEach(function(item){
+		      info += `
+		      <li>
+			        <h2>${item.name}</h2>
+			        <p>${item.summary}</p>
+			      </li>
+			        `;
+		    });
+
+	  res.send(`
+		    <h1>Roux Academy Meetups</h1>
+			    ${info}
+		    `);
+});
+
+var server = app.listen(app.get('port'), function() {
+	  console.log('Listening on port' + app.get('port'));
+});
+
+// var http = require('http');
+// //
+// // var myServer = http.createServer(function(request, response) {
+// //   response.writeHead(200, {"Content-Type" : "text/html"});
+// //   response.write('<h1>Roux Meetups</h1>');
+// //   response.end();
+// // });
+// //
+// // myServer.listen(3000);
+// // console.log('Go to http://localhost:3000 on your browser');
+//
